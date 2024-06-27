7b8df18fbc9f72f8af4b8d2be229cfb4db21a2d4
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-06-27 21:44:40 +0500

    all finished

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,5 +19,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', index)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
